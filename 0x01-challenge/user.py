17b827e50037ee43f36d98723068f79195ebc88c
Martha Siiyabia Tong <marthasiiyatong@gmail.com>
2024-07-23 22:54:47 -0700

    0x01. Fix my code

---
+++ 0x01-challenge/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+User class
+"""
+
+
+class User():
+    """ Documentation """
+
+    def __init__(self):
+        """ Documentation """
+        self.__email = None
+
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
